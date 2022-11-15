fdf48c166b8522ef06228a839ece0b6bb5aa861e
benedict-vimdey <bekudzordzi001@st.ug.edu.gh>
2022-11-15 13:06:35 +0000

    Set up frontend with react/typescript + tailwind

---
+++ crm-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to CRM Project</p>
+    </div>
+  );
+}
+
+export default App;

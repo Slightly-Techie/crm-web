666d76c16a4ee5c29cfe2c36d0f87c8b6ada12c7
josh-boat365 <nyarko.boateng20@outlook.com>
2022-11-22 15:55:07 +0000

    #8 repo structure for frontend

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import stars from "../assets/icons/Stars.png";
+import rocket from "../assets/icons/big-blue-flying rocket.png";
+function Login(){
+    return(
+         <div className="bg-[#1E1E1E] text-white font-[Monolisa]">
+            <h1 className="text-center text-[35px] font-bold">This is the Login Page</h1>
+        </div>
+    )
+}
+export default Login
---
+++ src/pages/signup.tsx
@@ -0,0 +1,10 @@
+import stars from "../assets/icons/Stars.png";
+import rocket from "../assets/icons/big-blue-flying-rocket.png";
+function SignUp(){
+    return(
+        <div className="bg-[#1E1E1E] text-white font-[Monolisa]">
+            <h1 className="text-center text-[35px] font-bold">This is the SignUp Page</h1>
+        </div>
+    )
+}
+export default SignUp

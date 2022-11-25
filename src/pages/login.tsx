9ea585c09f42a4fa6789754dee9a760f771f195d
josh-boat365 <nyarko.boateng20@outlook.com>
2022-11-25 14:05:29 +0000

    #3 #5 - Authentication screens complete update 1

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,22 @@
 import stars from "../assets/icons/Stars.png";
 import rocket from "../assets/icons/big-blue-flying-rocket.png";
+import githubLogo from "../assets/icons/Github-logo.png";
+import googleLogo from "../assets/icons/Google-logo.png";
 import {Link} from "react-router-dom";
 function Login(){
     return(
-    <div className="flex bg-[#111111] text-white font-[Monolisa]  Login" >
+    <div className="flex bg-[#111111] text-white font-[Monolisa] max-h-full  Login" >
         <div className="w-1/2 border-solid border-r-[1px] border-[#353535] left">
-            <div className="ml-[139px] mt-[235px] mb-[235px]  div">
+            <div className="ml-[139px] mt-[260px]">
                 <img className='mb-5' src={stars} alt="" />
-                <p className='text-xs font-medium'>Welcome to the</p>
-                <p className='text-[25px] font-semibold'>SLightly Techie</p>
-                <p className='text-[25px] font-semibold'>Network</p>
+                <p className='text-[18px] font-medium'>Welcome to the</p>
+                <p className='text-[35px] font-semibold'>SLightly Techie</p>
+                <p className='text-[35px] font-semibold'>Network</p>
                 <img className='mt-5' src={rocket} alt="" />
             </div>
         </div>
         <div className="right w-1/2">
-            <div className="ml-[176px] mr-[176px] mt-[150px] mb-[150px] div">
+            <div className="ml-[176px] mr-[176px] mt-[140px] mb-[120px] div">
             <form
                 onSubmit={(e: React.SyntheticEvent) => {
                 e.preventDefault();
@@ -27,17 +29,29 @@ function Login(){
                 // etc...
                 }}
                 >
-                    <h3 className='text-[20px] '>Login To Your Account</h3>
+                    <h3 className='text-[20px] font-bold '>Login To Your Account</h3>
                 <div className="mt-[40px] mb-5">
-                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] w-[20rem] placeholder:text-[14px] p-1 focus:outline-none focus:border-white border-[1.8px]' type="email" name="email" placeholder='John Doe' />
+                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] h-[40px] w-[20rem] placeholder:text-[14px] pl-4 focus:outline-none focus:border-white border-[1.8px]' type="email" name="email" placeholder='Johndoe@slightytechie.io' />
                 </div>
                 <div className="mb-3">
-                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.5px] w-[20rem] placeholder:text-[14px] p-1 focus:outline-none focus:border-white border-[1.8px]' type="password" name="password" placeholder='Enter Your Password' />
+                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] h-[40px] w-[20rem] placeholder:text-[14px] pl-4 focus:outline-none focus:border-white border-[1.8px]' type="password" name="password" placeholder='Enter your password' />
                 </div>
-                <p className="text-center text-[#353535] text-[11px] font-bold">Forgot your <Link className="font-bold hover:text-gray-400" to='/' ><u>password?</u></Link></p>
-                <div>
-                   <input type="button" value="Login to your account" />
+                <p className="mb-3 text-center text-[#353535] text-[11px] font-bold">Forgot your <Link className="font-bold hover:text-gray-400" to='/' ><u>password?</u></Link></p>
+            
+                <button id="btn" role="btn" type="submit" >Login to your account</button>
+                        
+                <div className="mt-6 mb-6 flex justify-center mx-auto gap-[1.3rem]">
+                    <hr className="w-[2.5rem] border-[#353535]" />
+                    <p className="text-[#353535] text-[11px] font-bold">continue with social media</p>
+                    <hr className="w-[2.5rem] border-[#353535]" />
                 </div>
+
+                <div className="flex flex-col items-center gap-[1.2rem]">
+                    <button className="bg-[#3A3A3A] hover:bg-black rounded-sm flex items-center justify-center text-[13px] w-[20rem] pr-[16px] pl-[16px] h-[48px] w-[20rem] gap-2" type="submit"><img src={googleLogo} alt="Google logo png"/><p>Continue with Google</p></button>
+                    <button className="bg-[#3A3A3A] hover:bg-black rounded-sm flex items-center justify-center text-[13px] pr-[16px] pl-[16px] h-[48px] w-[20rem] gap-2" type="submit"><img src={githubLogo} alt="Github logo png"/><p>Continue with Github</p></button>
+                </div>
+                <p className="mt-7 text-center text-[12px]">Not registered? <Link to="/signup"><u className="font-bold hover:text-gray-400">create account</u></Link></p>
+                
             </form>
 
             </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,10 +1,65 @@
 import stars from "../assets/icons/Stars.png";
 import rocket from "../assets/icons/big-blue-flying-rocket.png";
+import githubLogo from "../assets/icons/Github-logo.png";
+import googleLogo from "../assets/icons/Google-logo.png";
+import {Link} from "react-router-dom";
 function SignUp(){
     return(
-        <div className="bg-[#1E1E1E] text-white font-[Monolisa]">
-            <h1 className="text-center text-[35px] font-bold">This is the SignUp Page</h1>
+    <div className="flex bg-[#111111] text-white font-[Monolisa]  max-h-full SignUp" >
+        <div className="w-1/2 border-solid border-r-[1px] border-[#353535] max-h-full left">
+            <div className="ml-[139px] mt-[260px]">
+                <img className='mb-5' src={stars} alt="" />
+                <p className='text-[18px] font-medium'>Welcome to the</p>
+                <p className='text-[35px] font-semibold'>SLightly Techie</p>
+                <p className='text-[35px] font-semibold'>Network</p>
+                <img className='mt-5' src={rocket} alt="" />
+            </div>
         </div>
+        <div className="right w-1/2">
+            <div className="ml-[176px] mr-[176px] mt-[140px] mb-[120px] div">
+            <form
+                onSubmit={(e: React.SyntheticEvent) => {
+                e.preventDefault();
+                const target = e.target as typeof e.target & {
+                    email: { value: string };
+                    password: { value: string };
+                };
+                const email = target.email.value; // typechecks!
+                const password = target.password.value; // typechecks!
+                // etc...
+                }}
+                >
+                    <h3 className='text-[20px] font-bold '>Create An Account</h3>
+                <div className="mt-[40px] mb-5">
+                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] h-[40px] w-[20rem] placeholder:text-[14px] pl-4 focus:outline-none focus:border-white border-[1.8px]' type="email" name="email" placeholder='Johndoe@slightytechie.io' />
+                </div>
+                <div className="mb-5">
+                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] h-[40px] w-[20rem] placeholder:text-[14px] pl-4 focus:outline-none focus:border-white border-[1.8px]' type="password" name="password" placeholder='Enter your password' />
+                </div>
+                <div className="mb-5">
+                    <input className='bg-[#1E1E1E] border-[#353535] rounded-sm border-[1.8px] h-[40px] w-[20rem] placeholder:text-[14px] pl-4 focus:outline-none focus:border-white border-[1.8px]' type="password" name="password" placeholder='Confirm your password' />
+                </div>
+                {/* <p className="mb-3 text-center text-[#353535] text-[11px] font-bold">Forgot your <Link className="font-bold hover:text-gray-400" to='/' ><u>password?</u></Link></p> */}
+            
+                <button id="btn" role="btn" type="submit" >Create your account</button>
+                        
+                <div className="mt-6 mb-6 flex justify-center mx-auto gap-[1.3rem]">
+                    <hr className="w-[2.5rem] border-[#353535]" />
+                    <p className="text-[#353535] text-[11px] font-bold">continue with social media</p>
+                    <hr className="w-[2.5rem] border-[#353535]" />
+                </div>
+
+                <div className="flex flex-col items-center gap-[1.2rem]">
+                    <button className="bg-[#3A3A3A] hover:bg-black rounded-sm flex items-center justify-center text-[13px] w-[20rem] pr-[16px] pl-[16px] h-[48px] w-[20rem] gap-2" type="submit"><img src={googleLogo} alt="Google logo png"/><p>Continue with Google</p></button>
+                    <button className="bg-[#3A3A3A] hover:bg-black rounded-sm flex items-center justify-center text-[13px] pr-[16px] pl-[16px] h-[48px] w-[20rem] gap-2" type="submit"><img src={githubLogo} alt="Github logo png"/><p>Continue with Github</p></button>
+                </div>
+                <p className="mt-7 text-center text-[12px]">Already have an account? <Link to="/"><u className="font-bold hover:text-gray-400">Sign In</u></Link></p>
+                
+            </form>
+
+            </div>
+        </div>
+    </div>
     )
 }
 export default SignUp

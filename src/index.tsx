344d180061f00382e162f1be7a3b33f33ef0a698
CozyBrian <dragonbrian33@gmail.com>
2022-12-08 14:38:46 +0000

    removed unused boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 // routes
 import Login from "./pages/login";
@@ -25,8 +24,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

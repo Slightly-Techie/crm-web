c3233603c33d175a7411a414f1cb678a027719a7
Diabene <addodiabene69@gmail.com>
2023-05-04 23:16:17 +0000

    update: form functionality

--- src/pages/New-SignUp/NewSignUp.tsx
+++ src/pages/New-SignUp/NewSignUp.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import stars from "../../assets/icons/Stars.png";
 import rocket from "../../assets/icons/big-blue-flying-rocket.png";
-// import Profile from "./Profile";
-// import Social from "./Social";
-import Skills from "./Skills";
+import { NewSignUpFormData } from "./data";
+import useNavigateForms from "../../Hooks/useNavigateForms";
 
 function NewSignUp() {
+  const { currentForm, nextForm, previousForm, currentFormIndex } =
+    useNavigateForms(NewSignUpFormData);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="w-full bg-white dark:bg-[#111111]">
       <div className="w-screen h-screen grid lg:grid-cols-2 bg-[#Fff] dark:bg-[#111111] max-w-[1440px] mx-auto">
@@ -28,15 +34,28 @@ function NewSignUp() {
           </div>
         </div>
         <div className=" p-8 lg:w-5/6 mx-auto h-4/5 my-auto flex flex-col gap-4 ">
-          <Skills />
-          <section className="flex gap-4 justify-end">
-            <button className=" px-6 py-3 bg-[#001] dark:bg-white text-white dark:text-[#000] rounded-md">
-              Back
-            </button>
-            <button className=" px-6 py-3 bg-[#001] dark:bg-white text-white dark:text-[#000] rounded-md">
-              Next
-            </button>
+          <section className="flex text-[#000] dark:text-[#fff] mx-auto text-[1.5rem] font-medium justify-between">
+            <h3>{currentForm.heading}</h3>
           </section>
+          <form onSubmit={handleSubmit}>
+            {currentForm.element}
+            <section className="flex gap-4 justify-end">
+              {currentFormIndex > 0 && (
+                <button
+                  onClick={previousForm}
+                  className=" px-6 py-3 bg-[#001] dark:bg-white text-white dark:text-[#000] rounded-md"
+                >
+                  Back
+                </button>
+              )}
+              <button
+                onClick={nextForm}
+                className=" px-6 py-3 bg-[#001] dark:bg-white text-white dark:text-[#000] rounded-md"
+              >
+                {currentFormIndex === 2 ? "Submit" : "Next"}
+              </button>
+            </section>
+          </form>
         </div>
       </div>
     </div>
--- src/pages/New-SignUp/Profile.tsx
+++ src/pages/New-SignUp/Profile.tsx
@@ -3,55 +3,48 @@ import React from "react";
 function Profile() {
   return (
     <>
-      <section className="flex text-[#000] dark:text-[#fff] mx-auto text-[1.5rem] font-medium justify-between">
-        <h3>Profile</h3>
-      </section>
-      <section>
-        <form>
-          <div className=" my-4">
-            <label className="text-[#000] dark:text-[#fff]" htmlFor="">
-              Email
+      <div className=" my-4">
+        <label className="text-[#000] dark:text-[#fff]" htmlFor="">
+          Email
+        </label>
+        <input
+          className="w-full border-b-[1px]  text-[#000] dark:text-white border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
+          type="text"
+          placeholder="kofi@example.com"
+        />
+      </div>
+      <div className="my-4">
+        <label className="text-[#000] dark:text-white" htmlFor="">
+          Phone Number
+        </label>
+        <input
+          className=" text-[#000] dark:text-white w-full border-b-[1px] border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
+          type="text"
+        />
+      </div>
+      <div className="my-4">
+        <label className="text-[#000] dark:text-white" htmlFor="">
+          What type of techie are you?
+        </label>
+        <select className="w-full mt-4 border-b-[1px] text-[#000] dark:text-white border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]">
+          <option value="Backend">Backend</option>
+          <option value="Fronted">Frontend</option>
+          <option value="Full Stack">Full Stack</option>
+          <option value="UI/UX">UI/UX</option>
+          <option value="Mobile">Mobile</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>
+      {/* <div className="my-4">
+            <label className="text-[#000] dark:text-white" htmlFor="">
+            If Other above, please specify
             </label>
             <input
-              className="w-full border-b-[1px]  text-[#000] dark:text-white border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
-              type="text"
-              placeholder="kofi@example.com"
+            className="w-full border-b-[1px] text-[#000] dark:text-white border-b-[#33333380] disabled:border-b-[#33333357] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
+            type="text"
+            disabled
             />
-          </div>
-          <div className="my-4">
-            <label className="text-[#000] dark:text-white" htmlFor="">
-              Phone Number
-            </label>
-            <input
-              className=" text-[#000] dark:text-white w-full border-b-[1px] border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
-              type="text"
-            />
-          </div>
-          <div className="my-4">
-            <label className="text-[#000] dark:text-white" htmlFor="">
-              What type of techie are you?
-            </label>
-            <select className="w-full mt-4 border-b-[1px] text-[#000] dark:text-white border-b-[#33333380] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]">
-              <option value="Backend">Backend</option>
-              <option value="Fronted">Frontend</option>
-              <option value="Full Stack">Full Stack</option>
-              <option value="UI/UX">UI/UX</option>
-              <option value="Mobile">Mobile</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-          <div className="my-4">
-            <label className="text-[#000] dark:text-white" htmlFor="">
-              If Other above, please specify
-            </label>
-            <input
-              className="w-full border-b-[1px] text-[#000] dark:text-white border-b-[#33333380] disabled:border-b-[#33333357] input__transparent py-2 focus:outline-none focus:border-b-[1px] focus:border-b-[#333]"
-              type="text"
-              disabled
-            />
-          </div>
-        </form>
-      </section>
+          </div> */}
     </>
   );
 }

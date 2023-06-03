e416b71bed3acdd321b0377604a4c6d3f3059b7f
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-06-03 09:17:20 +0000

    Built the UI For Inactive Users

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,6 +6,7 @@ import PageNotFound from "../pages/404";
 import ForgotPassword from "../pages/auth/ForgotPassword/forgot-password";
 import { UserRoutes } from "./UserRoutes";
 import NewSignUp from "../pages/New-SignUp/NewSignUp";
+import InactiveUser from "../pages/inactive-user/InactiveUser";
 
 function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ function AppRoutes() {
       <Route path="/signup" element={<NewSignUp />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="*" element={<PageNotFound />} />
+      <Route path="/inactive-user" element={<InactiveUser/>}/>
     </Routes>
   );
 }

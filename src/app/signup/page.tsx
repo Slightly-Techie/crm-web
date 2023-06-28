6bb9cb235b82628a1e8dc6644272ac764de201dd
CozyBrian <dragonbrian33@gmail.com>
2023-06-28 22:36:01 +0000

    signup stack

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -29,8 +29,12 @@ export default function Signup() {
       const { years_of_experience } = NEW_USER_DATA;
       NEW_USER_DATA = {
         ...NEW_USER_DATA,
+        stack_id:
+          Number(NEW_USER_DATA.stack_id) === -1 ? 1 : NEW_USER_DATA.stack_id,
         years_of_experience: Number(years_of_experience),
       };
+      console.log(NEW_USER_DATA);
+
       createNewUser(NEW_USER_DATA);
     }
     next();

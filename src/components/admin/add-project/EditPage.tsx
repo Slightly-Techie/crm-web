2e66fb0fa9cd783b14f308d7fd5d59d1b17e0c96
emmens4u <emmanuel.mensah@amalitech.com>
2023-10-04 03:18:27 +0000

    Feat: Admin add project UI created

---
+++ src/components/admin/add-project/EditPage.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const EditPage = () => {
+  return (
+    <div className="w-2/3 h-[58rem] border border-gray-300 rounded-md ml-3">
+      <h1 className="text-2xl text-[#3D4450] border-b border-gray-300 font-bold p-3 ">
+        Slightly Techie CRM E-commerce
+      </h1>
+      <div className="flex flex-col gap-1 mt-4 mx-3">
+        <label className="text-[#3D4450] text-lg font-medium">
+          Name of Project
+        </label>
+        <input
+          type="text"
+          name="name-of-project"
+          className="border border-gray-300 rounded-md p-2 focus:outline-none bg-transparent"
+        />
+      </div>
+      <div className="flex flex-col gap-1 mt-4 mx-3">
+        <label className="text-[#3D4450] text-lg font-medium">
+          Project Description
+        </label>
+        <textarea
+          name="project description"
+          rows={6}
+          className="border border-gray-300 rounded-md p-2 resize-none focus:outline-none bg-transparent"
+        />
+      </div>
+      <div className="flex justify-evenly mt-5 mx-3 gap-4">
+        <div className="flex flex-col gap-1 w-full">
+          <label className="text-bold text-[#3D4450] font-medium">
+            Priorities
+          </label>
+          <select
+            name="priorities"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+          >
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-1 w-full">
+          <label className="text-bold text-[#3D4450] font-medium">
+            Priorities
+          </label>
+          <select
+            name="priorities"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+          >
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-1 w-full">
+          <label className="text-bold text-[#3D4450] font-medium">Size</label>
+          <select
+            name="priorities"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+          >
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+      </div>
+      <div className="mt-6 mx-3">
+        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+          <span>
+            <AiOutlinePlus className="text-[#3D4450]" />
+          </span>
+          Add Team Lead
+        </div>
+        <input
+          type="text"
+          name="add"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+        />
+      </div>
+      <div className="mt-6 mx-3">
+        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+          <span>
+            <AiOutlinePlus className="text-[#3D4450]" />
+          </span>
+          Add Frontend Team
+        </div>
+        <input
+          type="text"
+          name="add"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+        />
+      </div>
+      <div className="mt-6 mx-3">
+        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+          <span>
+            <AiOutlinePlus className="text-[#3D4450]" />
+          </span>
+          Add Backend Team
+        </div>
+        <input
+          type="text"
+          name="add"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+        />
+      </div>
+      <div className="flex gap-4 mt-6 mx-3">
+        <button className="bg-[#3D4450] w-1/2 py-2 text-white font-normal rounded-md">
+          Add Project
+        </button>
+        <button className="bg-white w-1/2 border border-[#3D4450] text-[#3D4450] font-medium rounded-md">
+          Draft Project
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EditPage;
---
+++ src/components/admin/add-project/Sidebar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const Sidebar = () => {
+  return (
+    <div className=" border border-gray-300 rounded-md w-1/5  h-[48rem]">
+      <div className="border border-3 border-gray-300 py-2 px-2 m-2 rounded-md">
+        <div className="flex items-center gap-3">
+          <AiOutlineSearch size={20} className="text-gray-400" />
+          <input
+            type="search"
+            name="search"
+            placeholder="Search Project"
+            className="w-full border-none outline-none bg-transparent"
+          />
+        </div>
+      </div>
+
+      <button className="bg-[#3D4450] text-white w-full rounded-md py-2">
+        Add Project
+      </button>
+      <div className="mt-5 flex flex-col cursor-pointer  gap-5">
+        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+          Slightly Techie CRM
+        </div>
+        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+          Slightly Techie Point System
+        </div>
+        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+          Slightly Techie CRM Design System
+        </div>
+        <div className="px-3 py-3 border-b border-gray-300 hover:bg-gray-500 hover:text-white">
+          New Project
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

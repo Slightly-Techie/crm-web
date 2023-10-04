893223ec2630fae4aceb62c46a240d67604e967d
emmens4u <emmanuel.mensah@amalitech.com>
2023-10-04 15:27:48 +0000

    fix: darkmode texts added

--- src/components/admin/add-project/EditPage.tsx
+++ src/components/admin/add-project/EditPage.tsx
@@ -3,38 +3,38 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 const EditPage = () => {
   return (
-    <div className="w-2/3 h-[58rem] border border-gray-300 rounded-md ml-3">
-      <h1 className="text-2xl text-[#3D4450] border-b border-gray-300 font-bold p-3 ">
+    <div className="w-2/3 h-[58rem] border border-gray-300 dark:border-gray-100 rounded-md ml-3">
+      <h1 className="text-2xl text-[#3D4450] dark:text-st-gray border-b border-gray-300 font-bold p-3 ">
         Slightly Techie CRM E-commerce
       </h1>
       <div className="flex flex-col gap-1 mt-4 mx-3">
-        <label className="text-[#3D4450] text-lg font-medium">
+        <label className="text-[#3D4450] dark:text-st-gray text-lg font-medium">
           Name of Project
         </label>
         <input
           type="text"
           name="name-of-project"
-          className="border border-gray-300 rounded-md p-2 focus:outline-none bg-transparent"
+          className="border border-gray-300 rounded-md dark:text-st-gray p-2 focus:outline-none bg-transparent"
         />
       </div>
       <div className="flex flex-col gap-1 mt-4 mx-3">
-        <label className="text-[#3D4450] text-lg font-medium">
+        <label className="text-[#3D4450] text-lg font-medium dark:text-st-gray">
           Project Description
         </label>
         <textarea
           name="project description"
           rows={6}
-          className="border border-gray-300 rounded-md p-2 resize-none focus:outline-none bg-transparent"
+          className="border border-gray-300 rounded-md p-2 resize-none focus:outline-none bg-transparent dark:text-st-gray"
         />
       </div>
       <div className="flex justify-evenly mt-5 mx-3 gap-4">
         <div className="flex flex-col gap-1 w-full">
-          <label className="text-bold text-[#3D4450] font-medium">
+          <label className="text-bold text-[#3D4450] font-medium dark:text-st-gray">
             Priorities
           </label>
           <select
             name="priorities"
-            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent dark:text-st-gray"
           >
             <option value="high">High</option>
             <option value="medium">Medium</option>
@@ -42,12 +42,12 @@ const EditPage = () => {
           </select>
         </div>
         <div className="flex flex-col gap-1 w-full">
-          <label className="text-bold text-[#3D4450] font-medium">
+          <label className="text-bold text-[#3D4450] font-medium dark:text-st-gray">
             Priorities
           </label>
           <select
             name="priorities"
-            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent dark:text-st-gray"
           >
             <option value="high">High</option>
             <option value="medium">Medium</option>
@@ -55,10 +55,12 @@ const EditPage = () => {
           </select>
         </div>
         <div className="flex flex-col gap-1 w-full">
-          <label className="text-bold text-[#3D4450] font-medium">Size</label>
+          <label className="text-bold text-[#3D4450] font-medium dark:text-st-gray">
+            Size
+          </label>
           <select
             name="priorities"
-            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent"
+            className="border border-gray-300 py-3 rounded-md focus:outline-none bg-transparent  dark:text-st-gray"
           >
             <option value="high">High</option>
             <option value="medium">Medium</option>
@@ -67,42 +69,42 @@ const EditPage = () => {
         </div>
       </div>
       <div className="mt-6 mx-3">
-        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+        <div className="text-bold text-[#3D4450] dark:text-st-gray font-medium flex items-center gap-2 cursor-pointer">
           <span>
-            <AiOutlinePlus className="text-[#3D4450]" />
+            <AiOutlinePlus className="text-[#3D4450] dark:text-st-gray" />
           </span>
           Add Team Lead
         </div>
         <input
           type="text"
           name="add"
-          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent dark:text-st-gray"
         />
       </div>
       <div className="mt-6 mx-3">
-        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+        <div className="text-bold text-[#3D4450] dark:text-st-gray font-medium flex items-center gap-2 cursor-pointer">
           <span>
-            <AiOutlinePlus className="text-[#3D4450]" />
+            <AiOutlinePlus className="text-[#3D4450] dark:text-st-gray" />
           </span>
           Add Frontend Team
         </div>
         <input
           type="text"
           name="add"
-          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent dark:text-st-gray"
         />
       </div>
       <div className="mt-6 mx-3">
-        <div className="text-bold text-[#3D4450] font-medium flex items-center gap-2 cursor-pointer">
+        <div className="text-bold text-[#3D4450] dark:text-st-gray font-medium flex items-center gap-2 cursor-pointer">
           <span>
-            <AiOutlinePlus className="text-[#3D4450]" />
+            <AiOutlinePlus className="text-[#3D4450] dark:text-st-gray" />
           </span>
           Add Backend Team
         </div>
         <input
           type="text"
           name="add"
-          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent"
+          className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:outline-none bg-transparent dark:text-st-gray"
         />
       </div>
       <div className="flex gap-4 mt-6 mx-3">
--- src/components/admin/add-project/Sidebar.tsx
+++ src/components/admin/add-project/Sidebar.tsx
@@ -3,8 +3,8 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 const Sidebar = () => {
   return (
-    <div className=" border border-gray-300 rounded-md w-1/5  h-[48rem]">
-      <div className="border border-3 border-gray-300 py-2 px-2 m-2 rounded-md">
+    <div className=" border border-gray-300 dark:border-gray-100 rounded-md w-1/5  h-[48rem]">
+      <div className="border border-3 border-gray-300 dark:border-gray-100 py-2 px-2 m-2 rounded-md">
         <div className="flex items-center gap-3">
           <AiOutlineSearch size={20} className="text-gray-400" />
           <input
@@ -16,20 +16,20 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <button className="bg-[#3D4450] text-white w-full rounded-md py-2">
+      <button className="bg-[#3D4450] dark:bg-white dark:text-st-text font-medium text-white w-full rounded-md py-2">
         Add Project
       </button>
-      <div className="mt-5 flex flex-col cursor-pointer  gap-5">
-        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+      <div className="mt-5 flex flex-col cursor-pointer gap-5">
+        <div className="border-b px-3 py-3 border-gray-300 dark:border-gray-100 hover:bg-gray-500 hover:text-white dark:text-st-gray">
           Slightly Techie CRM
         </div>
-        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white dark:text-st-gray">
           Slightly Techie Point System
         </div>
-        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white">
+        <div className="border-b px-3 py-3 border-gray-300 hover:bg-gray-500 hover:text-white dark:text-st-gray">
           Slightly Techie CRM Design System
         </div>
-        <div className="px-3 py-3 border-b border-gray-300 hover:bg-gray-500 hover:text-white">
+        <div className="px-3 py-3 border-b border-gray-300 hover:bg-gray-500 hover:text-white dark:text-st-gray">
           New Project
         </div>
       </div>

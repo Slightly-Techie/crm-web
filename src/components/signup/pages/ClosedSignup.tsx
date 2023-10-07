6825940cb39bc93e07f42c5289452080f00da46d
emmens4u <emmanuel.mensah@amalitech.com>
2023-10-07 23:25:11 +0000

    fix: date & links added

--- src/components/signup/pages/ClosedSignup.tsx
+++ src/components/signup/pages/ClosedSignup.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { HiCheckCircle } from "react-icons/hi2";
 import LinePng from "../../../assets/icons/Vector-27.png";
 import Image from "next/image";
+import { formatDate } from "@/utils";
 
 const ClosedSignup = () => {
+  const date = new Date(); // Current date
+  const formattedDate = formatDate(date);
   return (
     <div className="w-full h-full flex items-center justify-center text-black dark:text-white p-8 md:w-[35rem] font-tt-hoves mx-auto">
       <div className="relative w-full flex flex-col gap-6">
@@ -22,12 +25,22 @@ const ClosedSignup = () => {
           <p className="text-[#777] dark:font-medium text-lg">
             Don't worry though, we're constantly cooking up exciting
             opportunities! Keep an eye on our{" "}
-            <span className="text-[#FBBC04]">twitter page</span> and our
-            <span className="text-[#FBBC04]"> official website </span>
+            <a
+              href="https://twitter.com/_slightlyTechie"
+              className="text-[#FBBC04]"
+            >
+              twitter page
+            </a>{" "}
+            and our{" "}
+            <a
+              href="https://app.slightlytechie.com/"
+              className="text-[#FBBC04]"
+            >
+              official website{" "}
+            </a>
             for updates on when applications will swing back open. We can't wait
             to welcome you aboard.
           </p>
-
           <div className="mt-3">
             <Image src={LinePng} alt="line-png" />
           </div>
@@ -35,7 +48,7 @@ const ClosedSignup = () => {
             <HiCheckCircle size={20} className="text-[#FF6D64]" />
             <div className=" ">
               <p className="text">Application Closed</p>
-              <p className="text-[#777] text-">Mon May 29th, 2023</p>
+              <p className="text-[#777] text-">{formattedDate}</p>
               <p className="text-">Thank you for being awesome!</p>
             </div>
           </div>

eecc64cd0325d7efb829f42746c7bc8139b19ec3
Diabene <addodiabene69@gmail.com>
2023-10-21 19:23:00 +0000

    fix admin nav

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -14,9 +14,9 @@ export default async function RootLayout({
   }
 
   return (
-    <main className="font-tt-hoves bg-st-bg dark:bg-st-bgDark ">
+    <main className="font-tt-hoves bg-st-bg text-primary-dark dark:text-primary-light dark:bg-st-bgDark ">
       <Navbar />
-      <div className="p-8 min-h-screen max-w-screen-2xl mx-auto  ">
+      <div className="p-8 min-h-screen lg:pl-[25vw] xl:pl-[20vw]  w-full max-w-screen-2xl mx-auto  ">
         {children}
       </div>
     </main>

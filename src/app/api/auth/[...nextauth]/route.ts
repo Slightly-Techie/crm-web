7edc1c9d823c71f0795846effbb2f7f29979f969
NatMasetro <nathanielguggisberg@gmail.com>
2024-09-16 22:47:58 +0000

    AxiosAuth resolved

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,17 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      token?: string; // Add the token to the session user
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
